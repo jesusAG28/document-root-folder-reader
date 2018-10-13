a6e340b8585ada13785332571f9a004ac1d1597b
jesusAG <33084455+jesusAG28@users.noreply.github.com>
2018-10-13 20:07:16 +0200

    done

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+    <title> <?= $_SERVER['SERVER_NAME']; ?> </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./server_files/styles.css">
+</head>
+
+<body>
+<?php
+    require __DIR__ . '/server_files/server.php';
+?>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.html">Folders list</a>
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">GitHub</a>
+        </li>
+        <li class="nav-item">
+             <a class="nav-link" href="https://twitter.com/jesusayus">&#169; 2018 by Jesús Ayús</a>
+        </li>
+    </ul>
+  </nav>
+
+  <div class="container">
+        <!--
+        <nav class="navbar navbar-light bg-light justify-content-between mt-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="#" id="orderName">Order by name</a>
+                </li>
+                <li class="nav-item ml-1">
+                    <a class="nav-link" href="#" id="showOpen">Show open</a>
+                </li>
+            </ul>
+            <form class="form-inline mb-0">
+                <input class="form-control mr-sm-2" type="text" id="search" placeholder="Search" aria-label="Search">
+            </form>
+        </nav>
+        -->
+    <div id="foldersContainer" class="card-columns mt-4">
+        <?= $content ?>      
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ server_files/server.php
@@ -0,0 +1,69 @@
+<?php
+        $content ='';
+        $exts = array();
+        // Open directory
+        $myDirectory=opendir(".");
+        
+        // Get each entry
+        while($entryName=readdir($myDirectory)){
+            $dirArray[]=$entryName;
+        }
+
+        // Close directory
+        closedir($myDirectory);
+        
+        // Count elements in array
+        $indexCount=count($dirArray);
+        
+        // Sort files
+        sort($dirArray);
+        
+        // Loop through the array of files
+        for($index=0; $index < $indexCount; $index++) {
+        
+            // Allow ./?hidden to show hidden files
+            if($_SERVER['QUERY_STRING']=="hidden"){
+                $hide="";
+                $ahref="./";
+                $atext="Hide";
+            }else{
+                $hide=".";
+                $ahref="./?hidden";
+                $atext="Show";
+            }
+            
+            if(substr("$dirArray[$index]", 0, 1) != $hide) {
+            
+                // Gets File Names
+                $name=$dirArray[$index];
+                $namehref=$dirArray[$index];
+                
+                // Separates directories
+                if(is_dir($dirArray[$index])) {
+                    $extn="&lt;Directory&gt;"; 
+                    $class="dir";
+
+                    //Set the content to print, without the server files
+
+                    if($name!=='server_files'){
+                        $content=$content . "
+                            <div class='card'>
+                                <img class='card-img-top' src='./server_files/folder2.png'>
+                                <div class='card-body'>
+                                    <h5 class='card-title'>$name</h5>
+                                    <a href='./$namehref'><button class='mt-2 btn btn-sm btn-danger btn-delete'>Go To</button></a>
+                                </div>
+                                <div class='card-footer'>
+                                    <small class='text-muted'>".
+                                        $_SERVER["SERVER_NAME"]."/".$namehref
+                                    ."</small>
+                                </div>
+                            </div>
+                        ";
+                    }
+
+                }
+                
+            }
+        }
+    ?>
