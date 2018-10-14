795dca532059081ff9cc838e5c56babad6549394
agente jezu <jesusayus@gmail.com>
2018-10-14 13:45:24 +0200

    well done

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     <title> <?= $_SERVER['SERVER_NAME']; ?> </title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./server_files/styles.css">
+    <link rel="stylesheet" href="./server_files/style.css">
 </head>
 
 <body>
@@ -25,11 +23,12 @@
     </ul>
   </nav>
 
-  <div class="container">
-    <div id="foldersContainer" class="card-columns mt-4">
-        <?= $content ?>      
+    <div class="container-fluid">
+        <div class="card-columns">
+            <?= $content ?>
+        </div>
     </div>
-  </div>
+
 </body>
 
 </html>
