afcc6796e89fc89ad79c9edd9cb221486cfd571c
jesusAG <33084455+jesusAG28@users.noreply.github.com>
2018-10-15 16:32:45 +0200

    done again

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title> <?= $_SERVER['SERVER_NAME']; ?> </title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
